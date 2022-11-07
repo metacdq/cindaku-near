d8fbb42c147c2685fbf8d32be1e3e5264f681b59
Lion Father <me@alanmr.com>
2022-11-07 13:49:35 +0700

    add new nft contract

--- contract/build.sh
+++ contract/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+set -e
 echo ">> Building contract"
 
-near-sdk-js build src/contract.ts build/hello_near.wasm
+near-sdk-js build src/index.ts build/hello_near.wasm
---
+++ contract/src/index.ts
@@ -0,0 +1,125 @@
+
+import { NearBindgen, near, call, view, LookupMap, UnorderedMap, Vector, UnorderedSet } from 'near-sdk-js'
+import { NFTContractMetadata, Token, TokenMetadata, internalNftMetadata } from './metadata';
+import { internalMint } from './mint';
+import { internalNftTokens, internalSupplyForOwner, internalTokensForOwner, internalTotalSupply } from './enumeration';
+import { internalNftToken, internalNftTransfer, internalNftTransferCall, internalResolveTransfer } from './nft_core';
+import { internalNftApprove, internalNftIsApproved, internalNftRevoke, internalNftRevokeAll } from './approval';
+import { internalNftPayout, internalNftTransferPayout } from './royalty';
+
+/// This spec can be treated like a version of the standard.
+export const NFT_METADATA_SPEC = "nft-1.0.0";
+
+/// This is the name of the NFT standard we're using
+export const NFT_STANDARD_NAME = "nep171";
+
+@NearBindgen({})
+export class Contract {
+    owner_id: string;
+    tokensPerOwner: LookupMap<Token>;
+    tokensById: LookupMap<Token>;
+    tokenMetadataById: UnorderedMap<TokenMetadata>;
+    metadata: NFTContractMetadata;
+
+
+    constructor({
+        owner_id, 
+        metadata = {
+            spec: "nft-1.0.0",
+            name: "NFT Tutorial Contract",
+            symbol: "GOTEAM"
+        } 
+    }) {
+        this.owner_id = owner_id;
+        this.tokensPerOwner = new LookupMap("tokensPerOwner");
+        this.tokensById = new LookupMap("tokensById");
+        this.tokenMetadataById = new UnorderedMap("tokenMetadataById");
+        this.metadata = metadata;
+    }
+
+    default() {
+        return new Contract({owner_id: ''})
+    }
+
+    @call({})
+    nft_mint({ token_id, metadata, receiver_id, perpetual_royalties }) {
+        return internalMint({ contract: this, tokenId: token_id, metadata: metadata, receiverId: receiver_id, perpetualRoyalties: perpetual_royalties });
+    }
+
+    @view({})
+    nft_token({ token_id }) {
+        return internalNftToken({ contract: this, tokenId: token_id });
+    }
+
+    @call({})
+    nft_transfer({ receiver_id, token_id, approval_id, memo }) {
+        return internalNftTransfer({ contract: this, receiverId: receiver_id, tokenId: token_id, approvalId: approval_id, memo: memo });
+    }
+
+    @call({})
+    nft_transfer_call({ receiver_id, token_id, approval_id, memo, msg }) {
+        return internalNftTransferCall({ contract: this, receiverId: receiver_id, tokenId: token_id, approvalId: approval_id, memo: memo, msg: msg });
+    }
+
+    @call({})
+    nft_resolve_transfer({ authorized_id, owner_id, receiver_id, token_id, approved_account_ids, memo }) {
+        return internalResolveTransfer({ contract: this, authorizedId: authorized_id, ownerId: owner_id, receiverId: receiver_id, tokenId: token_id, approvedAccountIds: approved_account_ids, memo: memo });
+    }
+
+   
+    @view({})
+    nft_is_approved({ token_id, approved_account_id, approval_id }) {
+        return internalNftIsApproved({ contract: this, tokenId: token_id, approvedAccountId: approved_account_id, approvalId: approval_id });
+    }
+
+    @call({})
+    nft_approve({ token_id, account_id, msg }) {
+        return internalNftApprove({ contract: this, tokenId: token_id, accountId: account_id, msg: msg });
+    }
+
+
+    @view({})
+    nft_payout({ token_id, balance, max_len_payout }) {
+        return internalNftPayout({ contract: this, tokenId: token_id, balance: balance, maxLenPayout: max_len_payout });
+    }
+
+    @call({})
+    nft_transfer_payout({ receiver_id, token_id, approval_id, memo, balance, max_len_payout }) {
+        return internalNftTransferPayout({ contract: this, receiverId: receiver_id, tokenId: token_id, approvalId: approval_id, memo: memo, balance: balance, maxLenPayout: max_len_payout });
+    }
+
+    @call({})
+    nft_revoke({ token_id, account_id }) {
+        return internalNftRevoke({ contract: this, tokenId: token_id, accountId: account_id });
+    }
+
+    @call({})
+    nft_revoke_all({ token_id }) {
+        return internalNftRevokeAll({ contract: this, tokenId: token_id });
+    }
+
+    @view({})
+    nft_total_supply() {
+        return internalTotalSupply({ contract: this });
+    }
+
+    @view({})
+    nft_tokens({ from_index, limit }) {
+        return internalNftTokens({ contract: this, fromIndex: from_index, limit: limit });
+    }
+
+    @view({})
+    nft_tokens_for_owner({ account_id, from_index, limit }) {
+        return internalTokensForOwner({ contract: this, accountId: account_id, fromIndex: from_index, limit: limit });
+    }
+
+    @view({})
+    nft_supply_for_owner({ account_id }) {
+        return internalSupplyForOwner({ contract: this, accountId: account_id });
+    }
+
+    @view({})
+    nft_metadata() {
+        return internalNftMetadata({ contract: this });
+    }
+}
